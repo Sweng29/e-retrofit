8edb00826ec588738d2d1b5fe264caa9974869fc
Kashif Ali <kashif.ali@venturedive.com>
2021-03-19 00:32:49 +0500

    Initial commit

---
+++ src/main/java/com/retrofit/app/ERetrofitApplication.java
@@ -0,0 +1,13 @@
+package com.retrofit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERetrofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERetrofitApplication.class, args);
+	}
+
+}
